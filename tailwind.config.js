3a623930210297f34f7ae27876aae0b21677faeb
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-10-03 00:46:33 +0300

    update tailwind configuration - not mobile first

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,16 @@ module.exports = {
   ],
   darkMode: 'class',
   theme: {
+    screens: {
+      '2xl': {'min': '1535px'},
+      'xl': {'max': '1279px'},
+      'lg': {'max': '1023px'},
+      'md': {'max': '767px'},
+      'sm': {'max': '639px'},
+    },
     container: {
-      center: true
+      center: true,
+      padding: '1rem',
     },
     fontFamily: {
       'Russo-one': ['Russo One', 'sans-serif'],
