73e326a4c66cd2e115e25284faec91ac97d18192
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-10-03 00:49:13 +0300

    update _app: conditional rendering by global state

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,13 +1,29 @@
-import { Intro ,Nav } from '../components'
+import Head from 'next/head'
+import dynamic from 'next/dynamic'
+import { useCurrentStore } from '../store'
 import '../styles/globals.css'
 
 
 const MyApp = ({ Component, pageProps }) => {
+
+  const currentRace = useCurrentStore(state => state.currentRace)
+  const Intro = dynamic(() => import('../components/Intro'), { ssr: false })
+  const Nav = dynamic(() => import('../components/Nav'), { ssr: false })
+
+  
   return (
     <>
-      <Intro />
-      <Nav />
-      <Component {...pageProps} />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>F1 box</title>
+      </Head>
+
+    { !currentRace ? <Intro /> :
+      <>
+        <Nav />
+        <Component {...pageProps} />
+      </>
+    }
     </>
   )
 }
