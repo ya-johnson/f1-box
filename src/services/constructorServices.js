595c20304bc2068197c70f843ab24e25362451b4
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-10-07 18:59:35 +0300

    update (1) drivers and constructors services

--- src/services/constructorServices.js
+++ src/services/constructorServices.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 
 const getAllConstructors = async () => {
-  const response = await axios.get(`${API_URL}/constructors.json?=123`)
+  const response = await axios.get(`${API_URL}/constructors.json?=123&limit=1000`)
   const data = await response.data.MRData.ConstructorTable.Constructors
   const constructors = data.map(constructor => {
     return {
@@ -30,8 +30,146 @@ const getSeasonConstructors = async (season) => {
   return constructors
 }
 
+const getAllConstructorResults = async (constructor) => {
+  const response = await axios.get(`${API_URL}/constructors/${constructor}/results.json?limit=2000`)
+  const data = await response.data.MRData.RaceTable.Races.Results
+  const results = data.map(result => {
+    return {
+      constructor: `${result.Driver.givenName} ${result.Driver.familyName}`,
+      constructor: result.Constructor.name,
+      number: result.number,
+      position: result.position,
+      points: result.position,
+      grid: result.grid,
+      laps: result.laps,
+      status: result.status,
+      time: result.Time.time,
+      fastestLap: {
+        rank: result.FastestLap.rank,
+        lap: result.FastestLap.lap,
+        time: result.FastestLap.Time.time
+      },
+      averageSpeed: result.AverageSpeed.speed
+    }
+  })
+
+  return results
+}
+
+const getSeasonConstructorResults = async (season, constructor) => {
+  const response = await axios.get(`${API_URL}/${season}/constructors/${constructor}/results.json`)
+  const data = await response.data.MRData.RaceTable.Races.Results
+  const results = data.map(result => {
+    return {
+      constructor: `${result.Driver.givenName} ${result.Driver.familyName}`,
+      constructor: result.Constructor.name,
+      number: result.number,
+      position: result.position,
+      points: result.position,
+      grid: result.grid,
+      laps: result.laps,
+      status: result.status,
+      time: result.Time.time,
+      fastestLap: {
+        rank: result.FastestLap.rank,
+        lap: result.FastestLap.lap,
+        time: result.FastestLap.Time.time
+      },
+      averageSpeed: result.AverageSpeed.speed
+    }
+  })
+
+  return results
+}
+
+const getRoundConstructorResults = async (season, round, constructor) => {
+  const response = await axios.get(`${API_URL}/${season}/${round}/constructors/${constructor}/results.json`)
+  const data = await response.data.MRData.RaceTable.Races.Results
+  const results = data.map(result => {
+    return {
+      constructor: `${result.Driver.givenName} ${result.Driver.familyName}`,
+      constructor: result.Constructor.name,
+      number: result.number,
+      position: result.position,
+      points: result.position,
+      grid: result.grid,
+      laps: result.laps,
+      status: result.status,
+      time: result.Time.time,
+      fastestLap: {
+        rank: result.FastestLap.rank,
+        lap: result.FastestLap.lap,
+        time: result.FastestLap.Time.time
+      },
+      averageSpeed: result.AverageSpeed.speed
+    }
+  })
+
+  return results
+}
+
+const getAllConstructorQualify = async (constructor) => {
+  const response = await axios.get(`${API_URL}/constructors/${constructor}/qualifying.json?limit=2000`)
+  const data = await response.data.MRData.RaceTable.Races.QualifyingResults
+  const results = data.map(result => {
+    return {
+      constructor: `${result.Driver.givenName} ${result.Driver.familyName}`,
+      constructor: result.Constructor.name,
+      number: result.number,
+      position: result.position,
+      Q1: result.Q1,
+      Q2: result.Q2,
+      Q3: result.Q3
+    }
+  })
+
+  return results
+}
+
+const getSeasonConstructorQualify = async (season, constructor) => {
+  const response = await axios.get(`${API_URL}/${season}/constructors/${constructor}/qualifying.json`)
+  const data = await response.data.MRData.RaceTable.Races.QualifyingResults
+  const results = data.map(result => {
+    return {
+      constructor: `${result.Driver.givenName} ${result.Driver.familyName}`,
+      constructor: result.Constructor.name,
+      number: result.number,
+      position: result.position,
+      Q1: result.Q1,
+      Q2: result.Q2,
+      Q3: result.Q3
+    }
+  })
+
+  return results
+}
+
+const getRoundConstructorQualify = async (season, round, constructor) => {
+  const response = await axios.get(`${API_URL}/${season}/${round}/constructors/${constructor}/qualifying.json`)
+  const data = await response.data.MRData.RaceTable.Races.QualifyingResults
+  const results = data.map(result => {
+    return {
+      constructor: `${result.Driver.givenName} ${result.Driver.familyName}`,
+      constructor: result.Constructor.name,
+      number: result.number,
+      position: result.position,
+      Q1: result.Q1,
+      Q2: result.Q2,
+      Q3: result.Q3
+    }
+  })
+
+  return results
+}
+
 
 export {
   getAllConstructors,
-  getSeasonConstructors
+  getSeasonConstructors,
+  getAllConstructorResults,
+  getSeasonConstructorResults,
+  getRoundConstructorResults,
+  getAllConstructorQualify,
+  getSeasonConstructorQualify,
+  getRoundConstructorQualify
 }
--- src/services/driverServices.js
+++ src/services/driverServices.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 
 const getAllDrivers = async () => {
-  const response = await axios.get(`${API_URL}/drivers.json?=123`)
+  const response = await axios.get(`${API_URL}/drivers.json?=123&limit=2000`)
   const data = await response.data.MRData.DriverTable.Drivers
   const drivers = data.map(driver => {
     return {
@@ -36,8 +36,146 @@ const getSeasonDrivers = async (season) => {
   return drivers
 }
 
+const getAllDriverResults = async (driver) => {
+  const response = await axios.get(`${API_URL}/drivers/${driver}/results.json?limit=2000`)
+  const data = await response.data.MRData.RaceTable.Races.Results
+  const results = data.map(result => {
+    return {
+      driver: `${result.Driver.givenName} ${result.Driver.familyName}`,
+      constructor: result.Constructor.name,
+      number: result.number,
+      position: result.position,
+      points: result.position,
+      grid: result.grid,
+      laps: result.laps,
+      status: result.status,
+      time: result.Time.time,
+      fastestLap: {
+        rank: result.FastestLap.rank,
+        lap: result.FastestLap.lap,
+        time: result.FastestLap.Time.time
+      },
+      averageSpeed: result.AverageSpeed.speed
+    }
+  })
+
+  return results
+}
+
+const getSeasonDriverResults = async (season, driver) => {
+  const response = await axios.get(`${API_URL}/${season}/drivers/${driver}/results.json`)
+  const data = await response.data.MRData.RaceTable.Races.Results
+  const results = data.map(result => {
+    return {
+      driver: `${result.Driver.givenName} ${result.Driver.familyName}`,
+      constructor: result.Constructor.name,
+      number: result.number,
+      position: result.position,
+      points: result.position,
+      grid: result.grid,
+      laps: result.laps,
+      status: result.status,
+      time: result.Time.time,
+      fastestLap: {
+        rank: result.FastestLap.rank,
+        lap: result.FastestLap.lap,
+        time: result.FastestLap.Time.time
+      },
+      averageSpeed: result.AverageSpeed.speed
+    }
+  })
+
+  return results
+}
+
+const getRoundDriverResults = async (season, round, driver) => {
+  const response = await axios.get(`${API_URL}/${season}/${round}/drivers/${driver}/results.json`)
+  const data = await response.data.MRData.RaceTable.Races.Results
+  const results = data.map(result => {
+    return {
+      driver: `${result.Driver.givenName} ${result.Driver.familyName}`,
+      constructor: result.Constructor.name,
+      number: result.number,
+      position: result.position,
+      points: result.position,
+      grid: result.grid,
+      laps: result.laps,
+      status: result.status,
+      time: result.Time.time,
+      fastestLap: {
+        rank: result.FastestLap.rank,
+        lap: result.FastestLap.lap,
+        time: result.FastestLap.Time.time
+      },
+      averageSpeed: result.AverageSpeed.speed
+    }
+  })
+
+  return results
+}
+
+const getAllDriverQualify = async (driver) => {
+  const response = await axios.get(`${API_URL}/drivers/${driver}/qualifying.json?limit=2000`)
+  const data = await response.data.MRData.RaceTable.Races.QualifyingResults
+  const results = data.map(result => {
+    return {
+      driver: `${result.Driver.givenName} ${result.Driver.familyName}`,
+      constructor: result.Constructor.name,
+      number: result.number,
+      position: result.position,
+      Q1: result.Q1,
+      Q2: result.Q2,
+      Q3: result.Q3
+    }
+  })
+
+  return results
+}
+
+const getSeasonDriverQualify = async (season, driver) => {
+  const response = await axios.get(`${API_URL}/${season}/drivers/${driver}/qualifying.json`)
+  const data = await response.data.MRData.RaceTable.Races.QualifyingResults
+  const results = data.map(result => {
+    return {
+      driver: `${result.Driver.givenName} ${result.Driver.familyName}`,
+      constructor: result.Constructor.name,
+      number: result.number,
+      position: result.position,
+      Q1: result.Q1,
+      Q2: result.Q2,
+      Q3: result.Q3
+    }
+  })
+
+  return results
+}
+
+const getRoundDriverQualify = async (season, round, driver) => {
+  const response = await axios.get(`${API_URL}/${season}/${round}/drivers/${driver}/qualifying.json`)
+  const data = await response.data.MRData.RaceTable.Races.QualifyingResults
+  const results = data.map(result => {
+    return {
+      driver: `${result.Driver.givenName} ${result.Driver.familyName}`,
+      constructor: result.Constructor.name,
+      number: result.number,
+      position: result.position,
+      Q1: result.Q1,
+      Q2: result.Q2,
+      Q3: result.Q3
+    }
+  })
+
+  return results
+}
+
 
 export {
   getAllDrivers,
-  getSeasonDrivers
+  getSeasonDrivers,
+  getAllDriverResults,
+  getSeasonDriverResults,
+  getRoundDriverResults,
+  getAllDriverQualify,
+  getSeasonDriverQualify,
+  getRoundDriverQualify
 }
