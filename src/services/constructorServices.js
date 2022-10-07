2b670c900156143ae2f98a8df0430c7e98fb46e1
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-10-07 15:27:25 +0300

    add initial drivers and constructors services

--- src/services/constructorServices.js
+++ src/services/constructorServices.js
@@ -1,14 +1,37 @@
-import { supabase } from '../utils'
+import { API_URL } from '../config'
+import axios from 'axios'
 
 
-const getConstructorId = async (ref) => {
-  const { data, error } = await supabase.from('Constructors')
-                                        .select('constructorId')
-                                        .eq('constructorRef', ref)
-  return data[0].constructorId
+const getAllConstructors = async () => {
+  const response = await axios.get(`${API_URL}/constructors.json?=123`)
+  const data = await response.data.MRData.ConstructorTable.Constructors
+  const constructors = data.map(constructor => {
+    return {
+      constructor: constructor.name,
+      nationality: constructor.nationality,
+      url: constructor.url
+    }
+  })
+
+  return constructors
+}
+
+const getSeasonConstructors = async (season) => {
+  const response = await axios.get(`${API_URL}/${season}/constructors.json`)
+  const data = await response.data.MRData.ConstructorTable.Constructors
+  const constructors = data.map(constructor => {
+    return {
+      constructor: constructor.name,
+      nationality: constructor.nationality,
+      url: constructor.url
+    }
+  })
+
+  return constructors
 }
 
 
 export {
-  getConstructorId
+  getAllConstructors,
+  getSeasonConstructors
 }
--- src/services/driverServices.js
+++ src/services/driverServices.js
@@ -1,14 +1,43 @@
-import { supabase } from '../utils'
+import { API_URL } from '../config'
+import axios from 'axios'
 
 
-const getDriverId = async (ref) => {
-  const { data, error } = await supabase.from('Drivers')
-                                        .select('driverId')
-                                        .eq('driverRef', ref)
-  return data[0].driverId
+const getAllDrivers = async () => {
+  const response = await axios.get(`${API_URL}/drivers.json?=123`)
+  const data = await response.data.MRData.DriverTable.Drivers
+  const drivers = data.map(driver => {
+    return {
+      driver: `${driver.givenName} ${driver.familyName}`,
+      number: driver.permanentNumber,
+      code: driver.code,
+      birth: driver.dateOfBirth,
+      nationality: driver.nationality,
+      url: driver.url
+    }
+  })
+
+  return drivers
+}
+
+const getSeasonDrivers = async (season) => {
+  const response = await axios.get(`${API_URL}/${season}/drivers.json`)
+  const data = await response.data.MRData.DriverTable.Drivers
+  const drivers = data.map(driver => {
+    return {
+      driver: `${driver.givenName} ${driver.familyName}`,
+      number: driver.permanentNumber,
+      code: driver.code,
+      birth: driver.dateOfBirth,
+      nationality: driver.nationality,
+      url: driver.url
+    }
+  })
+
+  return drivers
 }
 
 
 export {
-  getDriverId
+  getAllDrivers,
+  getSeasonDrivers
 }
