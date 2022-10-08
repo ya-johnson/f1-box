90a76eaa8ed1b26d29f8c483a697ff957e51e099
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-10-08 19:22:12 +0300

    add api endpoint for constructors per season

---
+++ src/pages/api/constructors/[season].js
@@ -0,0 +1,18 @@
+import { constructorService ,wikiService } from '../../../services'
+
+
+const handler = async (req, res) => {
+  const { season } = req.query
+  const constructorsData = await constructorService.getSeasonConstructors(season)
+  const constructors = await Promise.all(constructorsData.map(async constructor => {
+    return {
+      ...constructor,
+      image: await wikiService.getWikiImage(constructor.url)
+    }
+  }))
+
+  res.status(200).json(constructors)
+}
+
+
+export default handler
