7e37afd43f04086ab2f251a5d6ac7ca7f21fce2c
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-10-08 19:17:15 +0300

    add api endpoint for drivers per season

---
+++ src/pages/api/drivers/[season].js
@@ -0,0 +1,19 @@
+import { driverService ,wikiService } from '../../../services'
+
+
+const handler = async (req, res) => {
+  const { season } = req.query
+  console.log(season)
+  const driversData = await driverService.getSeasonDrivers(season)
+  const drivers = await Promise.all(driversData.map(async driver => {
+    return {
+      ...driver,
+      image: await wikiService.getWikiImage(driver.url)
+    }
+  }))
+
+  res.status(200).json(drivers)
+}
+
+
+export default handler
