830e52d56377e29cde7fdba10efd533d5de4a145
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-10-12 20:36:46 +0300

    update layout Info section Home page

--- src/lib/pages/Home/Info.jsx
+++ src/lib/pages/Home/Info.jsx
@@ -12,59 +12,49 @@ const Info = ({ schdule, lastRace }) => {
 
 
   return (
-    <section className="container mb-14">
-      <div className="mb-10">
-        <h2>F1 Data made Easy</h2>
-        <p>In depth Analytics about Races, Drivers, Constructors and more.<br/>
-           Easy to use, free to Download.
-        </p>
-      </div>
-      <div className="flex items-center justify-start space-x-8">
-        <div className="w-96 h-44">
-          <div className="flex items-center space-x-4 pb-4">
-            <BsGraphUp className="h-6 w-6"/>
-            <p className="text-xl font-medium">Race Weekend Analytics</p>
-          </div>
-          <p>
-            All the little details in one place, <br/>
-            See how results came to be.
+    <section className="container  mb-14">
+      <div>
+        <div className="text-center mb-10">
+          <h2>F1 Data made Easy</h2>
+          <p>In depth Analytics about Races, Drivers, Constructors and more.<br/>
+             Easy to use, all graphics are free to Download (no copyrights restrictions).
           </p>
         </div>
+        <div className="flex items-center justify-center space-x-20">
+          <div className="max-w-[340px] h-44">
+            <div className="flex items-center space-x-4 pb-4">
+              <BsGraphUp className="h-6 w-6"/>
+              <p className="text-xl font-medium">Race Weekend Analytics</p>
+            </div>
+            <p>
+              Discover how One standout over the other. <br/>
+              What makes One strong and who is likely to.
+            </p>
+          </div>
 
-        <div className="w-96 h-44">
-          <div className="flex items-center space-x-4 pb-4">
-            <BsArrowsCollapse className="h-6 w-6 rotate-90"/>
-            <p className="text-xl font-medium">Compare</p>
+          <div className="max-w-[340px] h-44">
+            <div className="flex items-center space-x-4 pb-4">
+              <BsArrowsCollapse className="h-6 w-6 rotate-90"/>
+              <p className="text-xl font-medium">Compare</p>
+            </div>
+            <p>
+              Discover how One standout over the other. <br/>
+              What makes One strong and who is likely to.
+            </p>
           </div>
-          <p>
-            Discover how One standout over the other. <br/>
-            What makes One strong and who is likely to break records.
-          </p>
+          <div className="max-w-[340px] h-44">
+            <div className="flex items-center space-x-4 pb-4">
+              <BsFlag className="h-6 w-6"/>
+              <p className="text-xl font-medium">Simulator</p>
+            </div>
+            <p>
+              Enroll full Race or Qualifying season. <br/>
+              Recap highlights on more abstract manner.
+            </p>
+          </div>
         </div>
       </div>
-      <div className="flex items-center justify-start space-x-8">
-        <div className="w-96 h-44">
-          <div className="flex items-center space-x-4 pb-4">
-            <BsFlag className="h-6 w-6"/>
-            <p className="text-xl font-medium">Simulator</p>
-          </div>
-          <p>
-            Enroll full Race or Qualifying season. <br/>
-            Recap highlights on more abstract manner.
-          </p>
-        </div>
 
-        <div className="w-96 h-44">
-          <div className="flex items-center space-x-4 pb-4">
-            <BsFlag className="h-6 w-6"/>
-            <p className="text-xl font-medium">Simulator</p>
-          </div>
-          <p>
-            Enroll full Race or Qualifying season. <br/>
-            Recap highlights on more abstract manner.
-          </p>
-        </div>
-      </div>
       <Swiper slidesPerView={6}
               spaceBetween={40}
               initialSlide={lastRace.round}
