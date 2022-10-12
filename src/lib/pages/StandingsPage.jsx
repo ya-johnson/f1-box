c45ffd582033ad0efe78f0f30a2ccd9fefb91503
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-10-12 21:52:52 +0300

    update standing page - added initial charts

--- src/lib/pages/StandingsPage.jsx
+++ src/lib/pages/StandingsPage.jsx
@@ -1,7 +1,9 @@
 import { useState, useEffect } from 'react'
 import { useGlobalStore } from '../../store'
 import { standingsService } from '../../services'
-import { Table, Loader } from '../ui'
+import { Dropdown , Loader, Table } from '../ui'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Pie } from 'react-chartjs-2'
 
 
 const StandingsPage = () => {
@@ -9,7 +11,30 @@ const StandingsPage = () => {
   const lastRace = useGlobalStore(state => state.lastRace)
   const [driverStandings, setDriverStandings] = useState()
   const [constructorStandings, setConstructorStandings] = useState()
+  const [driversChartData, setDriversChartData] = useState()
+  const [constructorsChartData, setConstructorsChartData] = useState()
   const [loading, setLoading] = useState(true)
+
+  const setChartData = (type, standings) => {
+    if (type === 'drivers') {
+      const data = {
+        labels: standings.map(driver => driver.driver),
+        datasets: [{
+          data: standings.map(driver => driver.points)
+        }]
+      }
+      return data
+    } 
+    else if (type === 'constructors') {
+      const data = {
+        labels: standings.map(constructor => constructor.constructor),
+        datasets: [{
+          data: standings.map(constructor => constructor.points)
+        }]
+      }
+      return data
+    }
+  }
 
   const getStandings = async () => {
     const driverStandings = await standingsService.getDriverStandings(lastRace.season, lastRace.round)
@@ -17,8 +42,12 @@ const StandingsPage = () => {
 
     setDriverStandings(driverStandings)
     setConstructorStandings(constructorStandings)
+    setDriversChartData(setChartData('drivers', driverStandings))
+    setConstructorsChartData(setChartData('constructors', constructorStandings))
     setLoading(false)
   }
+
+  ChartJS.register(ArcElement, Tooltip, Legend)
 
 
   useEffect(() => {
@@ -29,24 +58,42 @@ const StandingsPage = () => {
   return (
     <main>
     { loading ? <Loader /> :
-      <section className="container">
+      <section className="container my-20">
         <div className="flex flex-col">
+
           <div className="flex items-center space-x-4">
             <h1>Standings</h1>
             <p className="text-2xl">{`Season: ${lastRace.season} round: ${lastRace.round}`}</p>
           </div>
-          <div>
+
+          <div className="mb-10">
             <h2>Drivers</h2>
-            {driverStandings && <Table cols={Object.keys(driverStandings[0])} 
-                                      rows={driverStandings} 
-                                      width='w-full' />}
+            <div className="flex">
+              <Table cols={Object.keys(driverStandings[0])} 
+                     rows={driverStandings} 
+                     width={'min-w-[600px]'}/>
+              <div className="w-full flex justify-center">
+                <div className="w-2/3">
+                  <Pie data={driversChartData} />
+                </div>
+              </div>
+            </div>
           </div>
-          <div>
-          <h2>Constructors</h2>
-            {constructorStandings && <Table cols={Object.keys(constructorStandings[0])} 
-                                            rows={constructorStandings} 
-                                            width='w-full' />}
+
+          <div className="mb-10">
+            <h2>Constructors</h2>
+            <div className="flex">
+              <Table cols={Object.keys(constructorStandings[0])} 
+                     rows={constructorStandings} 
+                     width={'min-w-[400px]'}/>
+              <div className="w-full flex justify-center">
+                <div className="w-2/5">
+                  <Pie data={constructorsChartData} />
+                </div>
+              </div>
+            </div>
           </div>
+
         </div>
       </section>
     }
