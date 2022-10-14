0f2bb257851612d2c6174efada3bbf226a23fe32
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-10-14 16:22:01 +0300

    update season services - mapping function

--- src/services/seasonServices.js
+++ src/services/seasonServices.js
@@ -1,18 +1,11 @@
 import { API_URL } from '../config'
 import axios from 'axios'
 
-const getSeasonsList = async () => {
-  const response = await axios.get(`${API_URL}/seasons.json?limit=100`)
-  const data = await response.data.MRData.SeasonTable.Seasons
-  return data
-}
 
-const getSeasonSchdule = async (year) => {
-  const response = await axios.get(`${API_URL}/${!year ? 'current' : year}.json`)
-  const data = await response.data.MRData.RaceTable
-  const scedule = {
-    season: data.season,
-    schdule: data.Races.map(race => {
+const mapSchedule = (season) => {
+  const schedule = {
+    season: season.season,
+    schdule: season.Races.map(race => {
       const raceInfo = {
         season: race.season,
         round: race.round,
@@ -21,7 +14,7 @@ const getSeasonSchdule = async (year) => {
         circuit: race.Circuit.circuitName,
         country: race.Circuit.Location.country,
         city: race.Circuit.Location.locality,
-        data: race.date,
+        date: race.date,
         time: race.time,
         firstPractice: {
           date: race.FirstPractice.date,
@@ -52,7 +45,22 @@ const getSeasonSchdule = async (year) => {
     }) 
   }
 
-  return scedule
+  return schedule
+}
+
+const getSeasonsList = async () => {
+  const response = await axios.get(`${API_URL}/seasons.json?limit=100`)
+  const data = await response.data.MRData.SeasonTable.Seasons
+  const seasonsData = data.map(season => season.season)
+  const seasons = [...seasonsData].reverse()
+  return seasons
+}
+
+const getSeasonSchdule = async (year) => {
+  const response = await axios.get(`${API_URL}/${!year ? 'current' : year}.json`)
+  const data = await response.data.MRData.RaceTable
+  const schedule = mapSchedule(data)
+  return schedule
 }
 
 
