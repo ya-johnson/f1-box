9f5b6ae6d3062a44832205b0d4eb947b3b8058e1
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-10-17 21:47:33 +0300

    update driver & constructor services - add mapping function and initial all results/qualify

--- src/services/constructorServices.js
+++ src/services/constructorServices.js
@@ -1,12 +1,11 @@
 import { API_URL } from '../config'
+import { raceService,resultService, qualifyService } from '.'
+import { algs } from '../utils'
 import axios from 'axios'
-import { resultService, qualifyService } from '.'
 
 
-const getAllConstructors = async () => {
-  const response = await axios.get(`${API_URL}/constructors.json?=123&limit=1000`)
-  const data = await response.data.MRData.ConstructorTable.Constructors
-  const constructors = data.map(constructor => {
+const mapConstructors = (constructors) => {
+  const mappedConstructors = constructors.map(constructor => {
     return {
       constructor: constructor.name,
       constructorId: constructor.constructorId,
@@ -15,29 +14,29 @@ const getAllConstructors = async () => {
     }
   })
 
+  return mappedConstructors
+}
+
+const getAllConstructors = async () => {
+  const response = await axios.get(`${API_URL}/constructors.json?=123&limit=1000`)
+  const data = await response.data.MRData.ConstructorTable.Constructors
+  const constructors = mapConstructors(data)
   return constructors
 }
 
 const getSeasonConstructors = async (season) => {
   const response = await axios.get(`${API_URL}/${season}/constructors.json`)
   const data = await response.data.MRData.ConstructorTable.Constructors
-  const constructors = data.map(constructor => {
-    return {
-      constructor: constructor.name,
-      constructorId: constructor.constructorId,
-      nationality: constructor.nationality,
-      url: constructor.url
-    }
-  })
-
+  const constructors = mapConstructors(data)
   return constructors
 }
 
 const getAllConstructorResults = async (constructor) => {
   const response = await axios.get(`${API_URL}/constructors/${constructor}/results.json?limit=2000`)
-  const data = await response.data.MRData.RaceTable.Races.Results
-  const results = resultService.mapResults(data)
-  return results
+  const data = await response.data.MRData.RaceTable.Races
+  const results = raceService.mapRaces(data, 'results')
+  const allResults = algs.groupByKey(results, 'season')
+  return allResults
 }
 
 const getSeasonConstructorResults = async (season, constructor) => {
@@ -57,8 +56,9 @@ const getRoundConstructorResults = async (season, round, constructor) => {
 const getAllConstructorQualify = async (constructor) => {
   const response = await axios.get(`${API_URL}/constructors/${constructor}/qualifying.json?limit=2000`)
   const data = await response.data.MRData.RaceTable.Races.QualifyingResults
-  const results = qualifyService.mapQualify(data)
-  return results
+  const qualify = raceService.mapRaces(data, 'qualify')
+  const allQualify = algs.groupByKey(qualify, 'season')
+  return allQualify
 }
 
 const getSeasonConstructorQualify = async (season, constructor) => {
--- src/services/driverServices.js
+++ src/services/driverServices.js
@@ -1,15 +1,14 @@
 import { API_URL } from '../config'
+import { raceService ,resultService, qualifyService } from '.'
+import { algs } from '../utils'
 import axios from 'axios'
-import { resultService, qualifyService } from '.'
 
 
-const getAllDrivers = async () => {
-  const response = await axios.get(`${API_URL}/drivers.json?=123&limit=2000`)
-  const data = await response.data.MRData.DriverTable.Drivers
-  const drivers = data.map(driver => {
+const mapDrivers = (drivers) => {
+  const mappedDrivers = drivers.map(driver => {
     return {
       driver: `${driver.givenName} ${driver.familyName}`,
-      driverId: driver.Driver.driverId,
+      driverId: driver.driverId,
       number: driver.permanentNumber,
       code: driver.code,
       birth: driver.dateOfBirth,
@@ -18,32 +17,29 @@ const getAllDrivers = async () => {
     }
   })
 
+  return mappedDrivers
+}
+
+const getAllDrivers = async () => {
+  const response = await axios.get(`${API_URL}/drivers.json?=123&limit=2000`)
+  const data = await response.data.MRData.DriverTable.Drivers
+  const drivers = mapDrivers(data)
   return drivers
 }
 
 const getSeasonDrivers = async (season) => {
   const response = await axios.get(`${API_URL}/${season}/drivers.json`)
   const data = await response.data.MRData.DriverTable.Drivers
-  const drivers = data.map(driver => {
-    return {
-      driver: `${driver.givenName} ${driver.familyName}`,
-      driverId: driver.Driver.driverId,
-      number: driver.permanentNumber,
-      code: driver.code,
-      birth: driver.dateOfBirth,
-      nationality: driver.nationality,
-      url: driver.url
-    }
-  })
-
+  const drivers = mapDrivers(data)
   return drivers
 }
 
 const getAllDriverResults = async (driver) => {
   const response = await axios.get(`${API_URL}/drivers/${driver}/results.json?limit=2000`)
-  const data = await response.data.MRData.RaceTable.Races.Results
-  const results = resultService.mapResults(data)
-  return results
+  const data = await response.data.MRData.RaceTable.Races
+  const results = raceService.mapRaces(data, 'results')
+  const allResults = algs.groupByKey(results, 'season')
+  return allResults
 }
 
 const getSeasonDriverResults = async (season, driver) => {
@@ -63,8 +59,9 @@ const getRoundDriverResults = async (season, round, driver) => {
 const getAllDriverQualify = async (driver) => {
   const response = await axios.get(`${API_URL}/drivers/${driver}/qualifying.json?limit=2000`)
   const data = await response.data.MRData.RaceTable.Races.QualifyingResults
-  const results = qualifyService.mapQualify(data)
-  return results
+  const qualify = raceService.mapRaces(data, 'qualify')
+  const allQualify = algs.groupByKey(qualify, 'season')
+  return allQualify
 }
 
 const getSeasonDriverQualify = async (season, driver) => {
