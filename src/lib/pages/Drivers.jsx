7d6fb39f23c5f8882f6bf9526a0c65201ca9ac38
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-10-21 17:30:28 +0300

    update Drivers component page - card style

--- src/lib/pages/Drivers.jsx
+++ src/lib/pages/Drivers.jsx
@@ -14,27 +14,32 @@ const Drivers = () => {
 
   const fetcher = url => axios.get(url).then(res => res.data)
   const { data, error } = useSwr(`api/drivers/grid/${season}`, fetcher)
-
   
   return (
     <main>
       <section className="container">
         <h1>Drivers</h1>
-        <Dropdown type='select'
+        <div>
+          <Dropdown type='select'
                     title='season:'
                     list={seasons}
                     defaultItem={currentSeason}
                     setItem={setSeason} />
+        </div>
       
       { !data && !error ? <Loader /> :
-        <div className="flex flex-wrap space-x-8 space-y-8 my-10">
+        <div className="grid grid-cols-4 xl:grid-cols-3 lg:grid-cols-2 sm:grid-cols-1 gap-20 my-10">
         { data.map(driver => {
           return (
-            <div className="card">
-              <img src={driver.image} className="w-[250px] h-[280px] object-center rounded-t-xl" />
-              <p>{driver.driver}</p>
-              <p>{driver.number}</p>
-              <p>{driver.birth}</p>
+            <div className="bg-neutral-300 dark:bg-neutral-700 rounded-xl">
+              <img src={driver.image} className="w-full h-[360px] object-center rounded-t-xl" />
+              <div className="h-max p-8 space-y-1 rounded-b-xl">
+                <Link href={driver.url} target="_blank">
+                  <a className="text-3xl font-400 hover:font-semibold duration-75">{driver.driver}</a>
+                </Link>
+                <p>{`Number: ${driver.number}`}</p>
+                <p>{`Date of Birth: ${driver.birth}`}</p>
+              </div>
             </div>
           )
         })}
