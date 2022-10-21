3dcedcd747feb5ffbdd6d74a0c803082ab67d993
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-10-21 22:39:27 +0300

    fix: query destructuring on races/[race] api endpoint

--- src/pages/api/races/[race].js
+++ src/pages/api/races/[race].js
@@ -2,7 +2,8 @@ import { resultService, qualifyService } from '../../../services'
 
 
 const handler = async (req, res) => {
-  const [season, round] = req.query.split('-')
+  const { race } = req.query
+  const [season, round] = race.split('-')
   const results = await resultService.getResults(season, round)
   const qualify = await qualifyService.getQualify(season, round)
   res.status(200).json({results, qualify})
