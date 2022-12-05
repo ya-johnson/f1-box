98d82af0b9d6b6107af7e48f45582f1e5a370154
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-12-05 20:07:25 +0200

    remove welcome message

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,10 +1,8 @@
-import { useEffect } from 'react'
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
 import { useGlobalStore } from '../store'
 import { Footer } from '../lib/components'
-import { ToastContainer, toast, Slide } from 'react-toastify'
-import { toastify } from '../utils'
+import { ToastContainer, Slide } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import '../styles/globals.css'
 
@@ -14,26 +12,6 @@ const MyApp = ({ Component, pageProps }) => {
   const nextRace = useGlobalStore(state => state.nextRace)
   const Intro = dynamic(() => import('../lib/components/Intro'), { ssr: false })
   const Nav = dynamic(() => import('../lib/components/Nav'), { ssr: false })
-
-  const Welcome = ({ closeToast, toastProps }) => (
-    <div className="p-2">
-      <p className="text-2xl font-bold mb-2">Welcome to F1 box!</p>
-      <p>We are still building some features, consider this app as unstable.</p>
-      <p>Data from seasons 1950-2000 may not be presented.</p>
-      <p>Thank you and happy racing!</p>  
-    </div>
-  )
-
-  const displayWelcome = () => {
-    toast(Welcome, toastify.openSettings)
-  }
-
-
-  useEffect(() => {
-    if (nextRace) {
-      displayWelcome()
-    }
-  }, [nextRace])
 
 
   return (
