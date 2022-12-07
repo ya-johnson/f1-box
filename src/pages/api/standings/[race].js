b9ae212b5af4c17788484d9753623ee6346237cf
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-12-07 12:50:03 +0200

    cleanup - standings api endpoint - remove sortChart function

--- src/pages/api/standings/[race].js
+++ src/pages/api/standings/[race].js
@@ -1,43 +1,19 @@
 import { standingsService } from '../../../services'
 
-const setChartData = (type, standings) => {
-  if (type === 'drivers') {
-    const data = standings.map(driver => {
-      return {
-        name: driver.driver,
-        points: parseInt(driver.points)
-      }
-    })
-
-    return data
-  } 
-  else if (type === 'constructors') {
-    const data = standings.map(constructor => {
-      return {
-        name: constructor.constructor,
-        points: parseInt(constructor.points)
-      }
-    })
-    
-    return data
-  }
-}
 
 const handler = async (req, res) => {
   const { race } = req.query
   const [season, round] = race.split('-')
   const driverStandings = await standingsService.getDriverStandings(season, round)
   const constructorStandings = await standingsService.getConstructorStandings(season, round)
-  const driverStandingsChart = setChartData('drivers', driverStandings)
-  const constructorStandingsChart = setChartData('constructors', constructorStandings)
   const data = {
     drivers: {
       standings: driverStandings,
-      chart: driverStandingsChart
+      chart: driverStandings.map(driver => ({name: driver.driver, points: parseInt(driver.points)}))
     },
     constructors: {
       standings: constructorStandings,
-      chart: constructorStandingsChart
+      chart: constructorStandings.map(constructor => ({name: constructor.constructor, points: parseInt(constructor.points)}))
     }
   }
   
