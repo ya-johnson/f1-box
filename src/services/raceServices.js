4397adbcab703f83b21547ed1f56a8d2b2a4a83f
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-12-08 14:04:09 +0200

    add mapRaces to raceService - handle chunck of races data

--- src/services/raceServices.js
+++ src/services/raceServices.js
@@ -3,6 +3,35 @@ import { resultService, qualifyService } from '.'
 import axios from 'axios'
 
 
+const getLastRace = async () => {
+  const response = await axios.get(`${API_URL}/current/last/results.json?limit=1`)
+  const data = await response.data.MRData.RaceTable.Races[0]
+  const lastRace = sortRaceInfo(data)
+  return lastRace
+}
+
+const getRace = async (season, round) => {
+  const response = await axios.get(`${API_URL}/${season}/${round}.json`)
+  const data = await response.data.MRData
+  const race = sortRaceInfo(data)
+  return race
+}
+
+const sortRaceInfo = (race) => {
+  const raceInfo = {
+    season: race.season,
+    round: race.round,
+    name: race.raceName,
+    circuitId: race.Circuit.circuitId,
+    circuitName: race.Circuit.circuitName,
+    country: race.Circuit.Location.country,
+    date: race.date,
+    time: race.time
+  }
+
+  return raceInfo
+}
+
 const mapRaces = (races, type) => {
   const mappedRaces = races.map(race => {
     const raceClean = {
@@ -16,11 +45,11 @@ const mapRaces = (races, type) => {
     }
 
     if (type === 'results') {
-      mappedRaces.results = resultService.mapResults(race.Results)
+      raceClean.results = resultService.mapResults(race.Results)
     }
 
     if (type === 'qualify') {
-      mappedRaces.qualifying = qualifyService.mapQualify(race.Qualifying)
+      raceClean.qualifying = qualifyService.mapQualify(race.Qualifying)
     }
 
     return raceClean
@@ -29,31 +58,9 @@ const mapRaces = (races, type) => {
   return mappedRaces
 }
 
-const getLastRace = async () => {
-  const response = await axios.get(`${API_URL}/current/last/results.json?limit=1`)
-  const data = await response.data.MRData.RaceTable.Races[0]
-  const lastRace = {
-    season: data.season,
-    round: data.round,
-    name: data.raceName,
-    country: data.Circuit.Location.country,
-    date: data.date,
-    time: data.time
-  }
-
-  return lastRace
-}
-
-const getRace = async (season, round) => {
-  const response = await axios.get(`${API_URL}/${season}/${round}.json`)
-  const data = await response.data.MRData
-  const race = data
-  return race
-}
-
 
 export {
-  mapRaces,
   getLastRace,
-  getRace
+  getRace,
+  mapRaces
 }
