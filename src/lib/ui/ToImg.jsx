3510e48d4855c5d919c4967347661ae572f47bca
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-12-11 09:37:17 +0200

    add ToImg component - download html parts

---
+++ src/lib/ui/ToImg.jsx
@@ -0,0 +1,36 @@
+import { useState, useRef } from 'react'
+import { toPng } from 'html-to-image'
+import { AiOutlineSave } from 'react-icons/ai'
+
+
+const ToImg = ({ children }) => {
+
+  const ref = useRef()
+  const [icon, setIcon] = useState('hidden')
+
+  const downloadAsImg = async () => {
+    ref.current.classList.add('p-4')
+    const dataUrl = await toPng(ref.current, {cacheBust: true})
+    const link = document.createElement('a')
+    link.download = 'f1-box.png'
+    link.href = dataUrl
+    link.click()
+    ref.current.classList.remove('p-4')
+  }
+
+  return (
+    <div className="relative bg-neutral-100 dark:bg-neutral-800"
+         ref={ref} 
+         onMouseOver={() => setIcon('block')} 
+         onMouseLeave={() => setIcon('hidden')}>
+      <AiOutlineSave className={`${icon} cursor-pointer absolute top-0 right-0 
+                                 h-5 w-5 text-neutral-500 
+                                 hover:text-neutral-800 dark:hover:text-neutral-200`} 
+                     onClick={downloadAsImg} />      
+      {children}
+    </div>
+  )
+}
+
+
+export default ToImg
