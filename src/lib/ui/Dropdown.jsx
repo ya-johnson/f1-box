02afedcee14f9e88fcdba90818a2d809ff371e5a
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-12-13 10:00:15 +0200

    fix remove item on multi selection - Dropdown

--- src/lib/ui/Dropdown.jsx
+++ src/lib/ui/Dropdown.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react'
+import { useState } from 'react'
 import { IoMdArrowDropdown, IoMdCloseCircleOutline } from 'react-icons/io'
 
 
@@ -35,12 +35,18 @@ const Dropdown = ({ type,
     }
   }
 
+  const removeMultiItem = (item) => {
+    setItem(selected.filter(listItem => listItem !== item))
+    setSelected(selected.filter(listItem => listItem !== item))
+    toggleDropdown()
+  }
+
 
   return (
     <div className="flex">
       <div className={`dropdown relative cursor-pointer mb-8
                        py-2 px-4 border brd rounded-lg
-                     bg-neutral-200 dark:bg-neutral-900 
+                       bg-neutral-200 dark:bg-neutral-900 
                       ${className && className} ${drop}`}
            tabIndex="0" 
            onClick={toggleDropdown}
@@ -52,50 +58,51 @@ const Dropdown = ({ type,
           {(type === 'multi' && selected.length > 0) && selected.map(item => {
             return (
               <div className="flex items-center pl-2 rounded-md
-                            bg-amber-400 text-neutral-800">
+                              bg-amber-400 text-neutral-800">
                 <p>{item}</p>
                 <div className="h-full ml-2 p-1 rounded-r-md 
-                              bg-neutral-300 dark:bg-neutral-700">
+                                bg-neutral-300 dark:bg-neutral-700">
                   <IoMdCloseCircleOutline className="h-5 w-5 hover:text-red-600" 
-                                          onClick={() => setSelected(selected.filter(listItem => listItem !== item))}/> 
+                                          onClick={() => removeMultiItem(item)}/> 
                 </div>     
               </div>)
           })}
+
           {(type === 'select' && selected) &&
             <div className="flex items-center pl-2 rounded-md
-                          bg-amber-400 text-neutral-800">
+                            bg-amber-400 text-neutral-800">
               <p>{selected}</p>
               <div className="h-full ml-2 p-1 rounded-r-md 
-                            bg-neutral-400 dark:bg-neutral-700">
+                              bg-neutral-400 dark:bg-neutral-700">
                 <IoMdCloseCircleOutline className="h-5 w-5 hover:text-red-600 dark:text-neutral-500" 
                                         onClick={() => setSelected(null)}/> 
               </div>     
             </div>
           }
 
-            { loading ?       
-              <div className="h-6 w-6 self-center animate-spin
-                              rounded-full border-4 border-solid 
+          {loading ?       
+            <div className="h-6 w-6 self-center animate-spin
+                            rounded-full border-4 border-solid 
                             border-neutral-800 border-t-neutral-300">
-              </div>
-              :
-              <IoMdArrowDropdown className={`h-6 w-6 ${drop === 'dd-close' ? 'rotate-0' : 'rotate-180'}`}/>
-            } 
+            </div>
+                   :
+            <IoMdArrowDropdown className={`h-6 w-6 ${drop === 'dd-close' ? 'rotate-0' : 'rotate-180'}`}/>
+          } 
         </div>
 
         <div className={`${drop === 'dd-close' && 'opacity-0 invisible'} 
                         absolute top-[125%] left-0 -translate-x-[1px] overflow-scroll
                         w-[calc(100%+2px)] max-h-[calc(100%*4+8px)] py-2 px-4 space-y-2
                         border brd rounded-lg bg-neutral-200 dark:bg-neutral-900 z-20`}>
-          { list.map((item, index) => {
-            return (
-              <div key={index} 
-                   className={`dd-item ${type === 'action' && item.className} ${item === selected && 'text-amber-400'}`}
-                   onClick={() => onItemClick(item)}>
-                  {type === 'select' || 'multi' ? item : item.name}
-              </div>
-            )
-          })}
+        {list.map((item, index) => {
+          return (
+            <div key={index} 
+                 className={`dd-item ${item === selected || selected.filter(listItem => listItem === item)[0] && 'text-amber-400'}`}
+                 onClick={() => onItemClick(item)}>
+                 {item}
+            </div>
+          )
+        })}
         </div>
       </div>
     </div>
