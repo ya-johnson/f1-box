43e68154c81370c0b71f9382a69338c8b1e94128
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2022-12-18 09:08:24 +0200

    fix constructor stats api endpoint - ergast api limit to 1000

--- src/pages/api/constructors/stats/[constructorId].js
+++ src/pages/api/constructors/stats/[constructorId].js
@@ -1,10 +1,30 @@
-import { constructorService } from '../../../../services'
+import { constructorService, wikiService } from '../../../../services'
 
 
 const handler = async (req, res) => {
   const { constructorId } = req.query
-  const stats = await constructorService.getAllconstructorResults(constructorId)
-  res.status(200).json(stats)
+  const constructor = await constructorService.getConstructorInfo(constructorId)
+  const image = await wikiService.getWikiImage(constructor.url)
+  const races = await constructorService.getConstructorRacesCount(constructorId)
+  const seasons = await constructorService.getAllConstructorSeasons(constructorId)
+  const championships = await constructorService.getConstructorChampionships(constructorId)
+  const podiums = await constructorService.getAllConstructorPodiums(constructorId)
+  const poles = await constructorService.getAllConstructorPoles(constructorId)
+  const results = await constructorService.getAllConstructorResults(constructorId)
+
+  const data = {
+    ...constructor,
+    image,
+    races,
+    seasons,
+    championships,
+    wins: podiums[0].length,
+    podiums: podiums[0].length + podiums[1].length + podiums[2].length,
+    poles,
+    results
+  }
+
+  res.status(200).json(data)
 }
 
 
