1f0f61e233e5280312149e0989d726e192002324
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2023-01-03 14:16:44 +0200

    update Table UI component - responsive

--- src/lib/ui/Table.jsx
+++ src/lib/ui/Table.jsx
@@ -1,28 +1,28 @@
 
 
-const Table = ({ cols, rows, width}) => {
+const Table = ({ cols, rows, width }) => {
 
   return (
-    <table className={`${width} mb-10 text-center border border-neutral-500 border-solid`}>
+    <table className={`${width && width} mb-10 mt-1 text-center border border-neutral-500 border-solid overflow-scroll`}>
       <thead>
         {cols.map(col => {
           return (
-            <th className="font-Kanit font-medium py-1 px-4 capitalize
-                         bg-neutral-300 dark:bg-neutral-700
-                           border border-neutral-500 border-solid">
+            <th className="font-Kanit font-semibold py-1 px-4 capitalize
+                           bg-white dark:bg-neutral-900
+                           border border-neutral-500 border-solid lg:px-2">
               {col}
             </th>
           )
         })}
       </thead>
-      <tbody>
+      <tbody className="bg-white dark:bg-neutral-900">
         {rows.map(row => {
           const rowItems = Object.values(row)
           return (
             <tr className="hover:text-amber-400">
               { rowItems.map(rowItem => {
                 return (
-                  <td className="py-1 px-4 border border-neutral-500 border-solid">{rowItem}</td>
+                  <td className="py-1 px-4 border border-neutral-500 border-solid lg:px-2">{rowItem}</td>
                 )
               })}
             </tr>
