f80a1720dd03157f16430321512b5ea30ccfe42a
Yonathan Abramov <70810810+ya-johnson@users.noreply.github.com>
2023-01-09 22:50:45 +0200

    update Loader - circle only

--- src/lib/ui/Loader.jsx
+++ src/lib/ui/Loader.jsx
@@ -2,12 +2,9 @@
 const Loader = () => {
 
   return (
-    <div className="w-screen h-screen flex items-center justify-center 
-                    fixed top-0 left-0 bg-neutral-500 bg-opacity-10 dark:bg-neutral-900 dark:bg-opacity-10">
-      <div className="loader h-10 w-10 self-center m-10 
-                      rounded-full border-4 border-solid 
-                      border-neutral-700 border-t-neutral-300 animate-spin">
-      </div>
+    <div className="absolute top-1/2 left-1/2 loader h-10 w-10 self-center m-10 
+                    rounded-full border-4 border-solid 
+                    border-neutral-700 border-t-neutral-300 animate-spin">
     </div>
 
   )
